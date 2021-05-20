8d0314ff635832d443d239145450cfa843c67d77
Brian Jacobo Fuentes <webdeveloper@pop-os.localdomain>
2021-05-20 14:22:25 -0500

    mi primer commit

---
+++ sesion_1/index.js
@@ -0,0 +1,74 @@
+//TIPADOS
+//String x = "manzanita";
+//Int y = 13;
+//Double z = 13.1;
+
+//NO TIPADOS
+// x = "manzanita";
+// y = 13;
+// z = 13.1;
+
+//********EN JAVASCRIPT NO ES OBLIGATORIO USAR ";" :D ***************
+
+//EN ESTE FRAGMENTO ESTAMOS DEFINIENDO VARIABLES DE TIPO NUMBER - ENTERO
+var x = 12;
+var y = 15;
+var z = 20;
+
+//EN ESTE FRAGMENTO ESTAMOS DEFINIENDO VARIABLES DE TIPO STRING(CADENA)
+var a = "Brian";
+var b = "Erick";
+var c = "Piero";
+
+//EN ESTE FRAGMENTO ESTAMOS DEFINIENDO VARIABLES DE TIPO DOUBLE(DECIMAL)
+var adrian = 18.5;
+var Alin = 21.5;
+var BIlly = 19.9;
+
+//EN ESTE FRAGMENTO ESTAMOS DEFINIENDO VARIABLES DE TIPO BOOLEAN(BOLEANAS)
+var mayo = true;
+var diciembre = false;
+
+var carvecitaHelada = true;
+var cervecitaCaliente = false;
+
+//EN ESTE FRAGMENTO ESTAMOS DEFINIENDO VARIABLES DE TIPO ARRAY(ARREGLO)
+var caja = [
+  "botella1",
+  "botella2",
+  "botella3",
+  "botella4",
+  "botella5",
+  "botella6",
+  "botella7",
+  x,
+  adrian,
+  alin,
+  mayo
+];
+
+var camion = [x,caja]
+//
+//EN ESTE FRAGMENTO ESTAMOS DEFINIENDO VARIABLES DE TIPO OBJECT(OBJETOS)
+var objeto1 = {
+    zapatilla:'nike',
+    consola:'playstation',
+    celulares:'xiaomi',
+    valorDolar:3.50
+}
+
+
+//TYPEOF(parametro)
+//me ayuda a reconocer que tipo de variable le estoy pasando como parametro
+
+//CONSOLE.LOG(pametro)
+//me imprime el valor pasado como parametro
+
+3-5---5
+4-6---6
+5-7---3
+6-8---2
+7-9---1
+
+//TAREA
+//[crear cuenta de github]
