f15013ddb306147a9dc43d1af3c62feeae2eb4d9
Brian Jacobo Fuentes <webdeveloper@pop-os.localdomain>
2021-05-22 16:40:09 -0500

    subiendo archivos sesion2V1

---
+++ sesion_2/index.js
@@ -0,0 +1,23 @@
+//1.Create a variable called carName, assign the value Volvo to it.
+//2.Create a variable called x, assign the value 50 to it.
+//3.Display the sum of 5 + 10, using two variables: x and y.
+//4.Create a variable called z, assign x + y to it, and display the result in an alert box.
+//5.On one single line, declare three variables with the following names and values:
+    // firstName = "John"
+    // lastName = "Doe"
+    // age = 35
+
+//Multiply 10 with 5, and alert the result:
+//Divide 10 by 2, and alert the result:
+//Alert the remainder when 15 is divided by 9
+//Use el operador de asignación correcto que resultará en que x sea 15
+//same as x = x + y
+
+//Alert "John" by extracting information from the person object.
+// var person = {
+//     firstName: "John",
+//     lastName: "Doe"
+//   };
+  
+//   alert(
+//   );
