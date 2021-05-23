d58774b812687a98a8cd6f579caca4c78ef6ce25
rdev <renato.monroy@ucsp.edu.pe>
2021-05-23 17:28:51 -0500

    Tarea 23/05/21

--- sesion_2/index.js
+++ sesion_2/index.js
@@ -6,26 +6,35 @@
 
 
 //1.Crear una variable llamada "carName" y asignarle el valor de "@tuNombre".
+let carName = "Renato M";
 //2.Create a variable called x, assign the value 50 to it.
-var x = 50
+let x = 50;
 //3.mostrar con un alert la suma de 20+5(a+b)
+let a = 5;
+let b = 7;
+alert(20 + 5 * (a + b));
 //4.Create a variable called z, y asignarle el valor de x+y y mostrar el resulado
+let y = 10;
+let z = x + y;
+console.log(z);
 //5.En una sola linea declarar lo siguiente:
     // firstName = "John"
     // lastName = "Doe"
     // age = 35
-
+let firstName = "Renato", lastName = "M", age = "24";
 //6.miltiplicar 2 numeros(x,y) y mostrarlos
+console.log(x * y);
 //7.Divide 10 y 2, mostrar resultados
+console.log(10 / 2);
 //8.mostrar el residuo de dividir 15/9
-
-
+console.log(15 % 9);
 //mostrar "John" usando el objeto person
-// var person = {
-//     firstName: "Brian",
-//     lastName: "Jacobo"
-//   };
-  
+var person = {
+    firstName: "Renato",
+    lastName: "M"
+};
+console.log(person.firstName);
+
 //   alert(
 //   );
 //0. git pull
