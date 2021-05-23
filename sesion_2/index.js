7116bdbd652d6f0eefe69033e2d4f31da433a705
yuyita-git <giulizina2305@gmail.com>
2021-05-23 17:52:32 -0500

    tarea sesion22

--- sesion_2/index.js
+++ sesion_2/index.js
@@ -5,19 +5,39 @@
 //9%2==>1
 
 
+//tarea resuelta
 //1.Crear una variable llamada "carName" y asignarle el valor de "@tuNombre".
+var carName="@Giuliana"
 //2.Create a variable called x, assign the value 50 to it.
-var x = 50
+    var x=50
 //3.mostrar con un alert la suma de 20+5(a+b)
+var a = 0
+var b = 5
+var suma = a + b
+alert(`la suma es ${suma}`)
+
 //4.Create a variable called z, y asignarle el valor de x+y y mostrar el resulado
+    var x=5
+    var y=2
+    var z=x+y
+
 //5.En una sola linea declarar lo siguiente:
     // firstName = "John"
     // lastName = "Doe"
     // age = 35
+var nombre="giuliana", apellido = "sahuarico", edad="29";
 
 //6.miltiplicar 2 numeros(x,y) y mostrarlos
+var x=2
+var y=3
+var z=x*y
 //7.Divide 10 y 2, mostrar resultados
+    var z=x/10 
+    var y=x/2 
 //8.mostrar el residuo de dividir 15/9
+    var x=15
+    var y=9
+    var residuo=x/y
 
 
 //mostrar "John" usando el objeto person
@@ -28,6 +48,11 @@ var x = 50
   
 //   alert(
 //   );
+var person = {
+    firstName: "Giuliana",
+    lastName: "Sahuarico"
+};
+alert(this.firstName + this.lastName);
 //0. git pull
     //git checkout -b rama-brianjacobo2
     //recien hacen sus cambios
