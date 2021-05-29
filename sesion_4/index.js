251ed975b485352456faf9563bd5b75d5f2678f8
Brian Jacobo Fuentes <webdeveloper@pop-os.localdomain>
2021-05-29 16:59:31 -0500

    cherry pick con cambios de Brian de origin a rama-rodrigodura

--- sesion_4/index.js
+++ sesion_4/index.js
@@ -1,15 +1,85 @@
-function miPrimeraFuncion(params) {
-  console.log("esta es mi primera función");
-}
+//**************functions()*****************//
+// function miPrimeraFuncion() {
+//     console.log("Hola esta es mi primera funcion")
+// }
+// miPrimeraFuncion()
 
-miPrimeraFuncion();
+// function  suma(x,y) {
+//     resultado = x+y
+//     return resultado    
+// }
+// console.log(suma(12,1));
+/******************************************/
 
-miPrimeraFuncion;
-function suma(x, y) {
-  resultado = x + y;
-  return resultado;
-}
+// const miPrimeraFuncion1 = () =>{
+//     console.log("Hola esta es mi primera funcion")
+// }
+// miPrimeraFuncion1()
 
-console.log(suma(12, 1));
+// const suma1 = (x,y)=>{
+//     var resultado = x + y
+//     return resultado
+// }
+// console.log(suma1(12,1));
 
-const miPrimeraFuncion2 = () => {};
+// var a = 20
+// var b = 40
+// var c = 7
+
+// //quiero obtener el mayor de 2 numeros ingresados sumado con 7
+// const numeroMayor = (a,b,c)=>{
+//     if (a>b) {
+//         return a + c
+//     }else if (a<b) {
+//         return b + c
+//     }else{
+//         return "los numeros son iguales"
+//     }
+// }
+// console.log(numeroMayor(a,b,c));
+
+
+//**************Common HTML Events**********//
+    // onchange
+    
+    // onclick
+    // const html = ()=>{
+    //     alert("este es un alert desde index.js")
+    // }
+    // // onmouseover
+    // const onMouseOver=()=>{
+    //     console.log("hola desde funcion onMouseOver()");
+    //     document.getElementById("test").innerHTML = " hola desde MOuse Over"
+    // }
+    // // onmouseout
+    // const onMouseOut=()=>{
+    //     console.log("hola desde funcion onMouseOut()");
+    // }
+    // // onkeydown
+    // const onKeyDownPaolo=()=>{
+    //     alert("hola desde onkeyDownPaolo")
+    // }
+    // // onload
+    // const onLoadRenato=()=>{
+    //     console.log("cargando onload")
+    // }
+//**************StringMethods***************//
+    // length()
+    var str = "hola mundo mundo mundo mundo gaseosa"
+    console.log(str.length);
+
+    // indexOf()
+    //console.log(str.indexOf("mundo"));
+    // lastIndex()
+    //console.log(str.lastIndexOf("mundo"));
+    // indexOf(x,y)
+    //console.log(str.indexOf("mundo",10));
+    // lastIndex(,)
+    console.log(str.lastIndexOf("mundo",10))
+    // search()-----TAREA
+    // slice(start, end)
+    console.log(str.slice(7,16))
+    // substring(start, end)
+    // The difference is that substring() cannot accept negative indexes.
+    // substr(start, length)
+    // The difference is that the second parameter specifies the length of the extracted part.
