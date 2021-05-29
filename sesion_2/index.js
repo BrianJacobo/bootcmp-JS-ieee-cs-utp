be6bd0b367eb03efd5b48d42b3eaf671bbcd8943
RodrigoD <rdurae@gmail.com>
2021-05-29 11:56:01 -0500

    tarea resuelta por rodrigo durá

--- sesion_2/index.js
+++ sesion_2/index.js
@@ -4,33 +4,67 @@
 //9/2==>4.5
 //9%2==>1
 
+///////////////////////////////////////////
+/////////////////TAREA/////////////////////
+//////////////////////////////////////////
 
 //1.Crear una variable llamada "carName" y asignarle el valor de "@tuNombre".
+
+var carName = "Rodrigo";
+
 //2.Create a variable called x, assign the value 50 to it.
-var x = 50
+
+var x = 50;
+
 //3.mostrar con un alert la suma de 20+5(a+b)
+
+var a = 20;
+var b = 5;
+
+alert(a + b);
+
 //4.Create a variable called z, y asignarle el valor de x+y y mostrar el resulado
+
+var z = x + y;
+console.log(z);
+
 //5.En una sola linea declarar lo siguiente:
-    // firstName = "John"
-    // lastName = "Doe"
-    // age = 35
+// firstName = "John"
+// lastName = "Doe"
+// age = 35
+
+var firstName = "John",
+  lastName = "Doe",
+  age = 35;
 
 //6.miltiplicar 2 numeros(x,y) y mostrarlos
+
+var x = 10;
+var y = 20;
+
+console.log(x * y);
+
 //7.Divide 10 y 2, mostrar resultados
+
+console.log(10 / 2);
+
 //8.mostrar el residuo de dividir 15/9
 
+console.log(15 % 9);
 
-//mostrar "John" usando el objeto person
-// var person = {
-//     firstName: "Brian",
-//     lastName: "Jacobo"
-//   };
-  
+//9. mostrar "John" usando el objeto person
+var person = {
+  firstName: "John"
+};
+
+console.log(person.firstName);
+
 //   alert(
 //   );
+
 //0. git pull
-    //git checkout -b rama-brianjacobo2
-    //recien hacen sus cambios
+//git checkout -b rama-brianjacobo2
+//recien hacen sus cambios
 //1. git add .
 //2. git commit - "tarea resuelta del 1 al 3"
 //3. git push
