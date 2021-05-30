69d9d3edd414142da27532412f7329d3efe362f1
RodrigoD <rdurae@gmail.com>
2021-05-29 21:25:19 -0500

    enviando cambios sesion 4

---
+++ sesion_4/index.js
@@ -0,0 +1,15 @@
+function miPrimeraFuncion(params) {
+  console.log("esta es mi primera función");
+}
+
+miPrimeraFuncion();
+
+miPrimeraFuncion;
+function suma(x, y) {
+  resultado = x + y;
+  return resultado;
+}
+
+console.log(suma(12, 1));
+
+const miPrimeraFuncion2 = () => {};
