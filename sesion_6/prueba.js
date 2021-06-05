a95475476f5e0b4da8c9d1f7c62170e12f7cce05
Brian Jacobo Fuentes <webdeveloper@pop-os.localdomain>
2021-06-05 13:24:37 -0500

    tarea sesion 6

--- sesion_6/prueba.js
+++
@@ -1,5 +0,0 @@
-function repeatStr (n, s) {
-    return s.repeat(n)
-  }
-
-  console.log(repeatStr(5,"p"))
--- sesion_6/string.js
+++
@@ -1,7 +0,0 @@
-var txt = "hola mundo desde escritorio";
-var txt1 = "desde aqui te puedo ver "
-
-arr1 = txt.replace(/ /g,"").split("")
-arr = txt1.trim().split(" ")
-
-console.log(arr);
