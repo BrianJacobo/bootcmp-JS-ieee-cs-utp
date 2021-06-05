1b3a68704af94b4a9d33dc06ef7607b190aa4d7e
Marcos1803 <marcosmatos180300@gmail.com>
2021-06-05 15:52:58 -0500

    Tarea 2 resuelta

--- sesion_2/index.js
+++ sesion_2/index.js
@@ -6,20 +6,38 @@
 
 
 //1.Crear una variable llamada "carName" y asignarle el valor de "@tuNombre".
+var carName = 'Marcos';
 //2.Create a variable called x, assign the value 50 to it.
+var x = 50;
 //3.mostrar con un alert la suma de 20+5(a+b)
+var a = 20;
+var b = 5;
+alert(a+b);
 //4.Create a variable called z, y asignarle el valor de x+y y mostrar el resulado
+var x = 20;
+var y = 5;
+var z = x+y;
+alert(z);
 //5.En una sola linea declarar lo siguiente:
     // firstName = "John"
     // lastName = "Doe"
     // age = 35
-
+var firstName = 'Jhon'; var lastName ='Doe'; var age = 35;
 //6.miltiplicar 2 numeros(x,y) y mostrarlos
+var x = 3;
+var y = 11;
+alert(x*y);
 //7.Divide 10 y 2, mostrar resultados
+var x = 12;
+var y = 3;
+alert(x/y);
 //8.mostrar el residuo de dividir 15/9
-
-
+var x = 11;
+var y = 3;
+alert(x%y);
 //mostrar "John" usando el objeto person
+var persona = {firstName:'Jhon',lastName:'Doe'};
+alert(persona.firstName);
 // var person = {
 //     firstName: "Brian",
 //     lastName: "Jacobo"
