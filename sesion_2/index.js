c45525e523e81bfff1b5e6f7ee09a7a5f6f085ee
Marcos1803 <marcosmatos180300@gmail.com>
2021-06-05 18:18:46 -0500

    Tarea 2

--- sesion_2/index.js
+++ sesion_2/index.js
@@ -14,8 +14,8 @@ var a = 20;
 var b = 5;
 alert(a+b);
 //4.Create a variable called z, y asignarle el valor de x+y y mostrar el resulado
-var x = 20;
-var y = 5;
+var x;
+var y;
 var z = x+y;
 alert(z);
 //5.En una sola linea declarar lo siguiente:
@@ -24,16 +24,16 @@ alert(z);
     // age = 35
 var firstName = 'Jhon'; var lastName ='Doe'; var age = 35;
 //6.miltiplicar 2 numeros(x,y) y mostrarlos
-var x = 3;
-var y = 11;
+var x;
+var y
 alert(x*y);
 //7.Divide 10 y 2, mostrar resultados
 var x = 12;
-var y = 3;
+var y = 2;
 alert(x/y);
 //8.mostrar el residuo de dividir 15/9
-var x = 11;
-var y = 3;
+var x = 15;
+var y = 9;
 alert(x%y);
 //mostrar "John" usando el objeto person
 var persona = {firstName:'Jhon',lastName:'Doe'};
