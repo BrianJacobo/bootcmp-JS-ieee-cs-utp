34b1b182ecbfcadf887214a2a4682e08131f49c7
RodrigoD <rdurae@gmail.com>
2021-06-06 01:06:57 -0500

    enviando cambios a mi rama

--- sesion_4/index.js
+++ sesion_4/index.js
@@ -1,68 +1,69 @@
 //**************functions()*****************//
-// function miPrimeraFuncion() {
-//     console.log("Hola esta es mi primera funcion")
-// }
-// miPrimeraFuncion()
+function miPrimeraFuncion() {
+    console.log("Hola esta es mi primera funcion")
+}
+miPrimeraFuncion()
 
-// function  suma(x,y) {
-//     resultado = x+y
-//     return resultado    
-// }
-// console.log(suma(12,1));
+function  suma(x,y) {
+    resultado = x+y
+    return resultado    
+}
+console.log(suma(12,1));
 /******************************************/
 
-// const miPrimeraFuncion1 = () =>{
-//     console.log("Hola esta es mi primera funcion")
-// }
-// miPrimeraFuncion1()
+const miPrimeraFuncion1 = () =>{
+    console.log("Hola esta es mi primera funcion")
+}
+miPrimeraFuncion1()
 
-// const suma1 = (x,y)=>{
-//     var resultado = x + y
-//     return resultado
-// }
-// console.log(suma1(12,1));
+const suma1 = (x,y)=>{
+    var resultado = x + y
+    return resultado
+}
+console.log(suma1(12,1));
 
-// var a = 20
-// var b = 40
-// var c = 7
+var a = 20
+var b = 40
+var c = 7
 
 // //quiero obtener el mayor de 2 numeros ingresados sumado con 7
-// const numeroMayor = (a,b,c)=>{
-//     if (a>b) {
-//         return a + c
-//     }else if (a<b) {
-//         return b + c
-//     }else{
-//         return "los numeros son iguales"
-//     }
-// }
-// console.log(numeroMayor(a,b,c));
+const numeroMayor = (a,b,c)=>{
+    if (a>b) {
+        return a + c
+    }else if (a<b) {
+        return b + c
+    }else{
+        return "los numeros son iguales"
+    }
+}
+console.log(numeroMayor(a,b,c));
 
 
 //**************Common HTML Events**********//
     // onchange
     
     // onclick
-    // const html = ()=>{
-    //     alert("este es un alert desde index.js")
-    // }
-    // // onmouseover
-    // const onMouseOver=()=>{
-    //     console.log("hola desde funcion onMouseOver()");
-    //     document.getElementById("test").innerHTML = " hola desde MOuse Over"
-    // }
-    // // onmouseout
-    // const onMouseOut=()=>{
-    //     console.log("hola desde funcion onMouseOut()");
-    // }
-    // // onkeydown
-    // const onKeyDownPaolo=()=>{
-    //     alert("hola desde onkeyDownPaolo")
-    // }
-    // // onload
-    // const onLoadRenato=()=>{
-    //     console.log("cargando onload")
-    // }
+    const html = ()=>{
+        alert("este es un alert desde index.js")
+    }
+    // onmouseover
+    const onMouseOver=()=>{
+        console.log("hola desde funcion onMouseOver()");
+        document.getElementById("test").innerHTML = " hola desde MOuse Over"
+    }
+    // onmouseout
+    const onMouseOut=()=>{
+        console.log("hola desde funcion onMouseOut()");
+    }
+    // onkeydown
+    const onKeyDownPaolo=()=>{
+        alert("hola desde onkeyDownPaolo")
+    }
+
+    // onload
+    const onLoadRenato=()=>{
+        console.log("cargando onload")
+    }
 //**************StringMethods***************//
     // length()
     var str = "hola mundo mundo mundo mundo gaseosa"
