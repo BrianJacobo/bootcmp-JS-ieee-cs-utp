040d2e78a4fdf819aa51ef977aaa4c86ea83246a
Brian Jacobo Fuentes <webdeveloper@pop-os.localdomain>
2021-06-12 15:52:27 -0500

    subiendo cambios a sesion 6 y subiendo archivos de inicializacion de la sesion7

--- sesion_6/const-let-var.js
+++ sesion_6/const-let-var.js
@@ -1,18 +1,30 @@
-const personaje = "spiderman"
+const personaje = "spiderman"//Global
 
 if (true) {
-    personaje = "hulk"
-    console.log(personaje);
+    const personaje = "hulk"//Local
+    console.log(personaje);//Local
 }
-console.log(personaje)
+console.log(personaje)//Global
 /******************************************** */
+/*EJEMPLO CON CONST*/
+// const adrian = 20
+// adrian = 21
+// console.log(adrian);
 
-let humano = "spiderman"
+/*EJEMPLO CON LET*/
+// let flor = 20
+// flor = 21
+// console.log(flor);
+
+const dni = 72017468
+let edad = edad +1
+
+let humano = "spiderman"//Global
 if (true) {
-    humano = "hulk"
-    console.log(humano);
+    humano = "hulk"//Local
+    console.log(humano);//Local
 }
-console.log(humano)
+console.log(humano)//Glocal
 
 // function y
 // function z
---
+++ sesion_7/index.js
@@ -0,0 +1,24 @@
+// ---------JavaScript Array Methods-----------
+// The JavaScript method toString() converts an array to a string of (comma separated) array values.*/
+const carros = ["carro1","carro2","carro3","carro4"]
+carrosToString = carros.toString()
+console.log(carrosToString);
+
+// The join() method also joins all array elements into a string.
+// It behaves just like toString(), but in addition you can specify the separator:
+console.log(carros.join("-"));
+
+// The pop() method removes the last element from an array:
+// The pop() method returns the value that was "popped out":
+
+
+// The push() method adds a new element to an array (at the end):
+
+// Shifting is equivalent to popping, working on the first element instead of the last.
+// The shift() method removes the first array element and "shifts" all other elements to a lower index.
+// The shift() method returns the string that was "shifted out":
+
+
+
+// The unshift() method adds a new element to an array (at the beginning), and "unshifts" older elements:
+
