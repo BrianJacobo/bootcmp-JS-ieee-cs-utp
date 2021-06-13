b3d095d7c2a1d8354616f4ff6401ccc38f83a49e
Adrian <adrian.capchaq@gmail.com>
2021-06-12 23:10:17 -0500

    subiendo tarea 2

--- sesion_2/index.js
+++ sesion_2/index.js
@@ -6,17 +6,28 @@
 
 
 //1.Crear una variable llamada "carName" y asignarle el valor de "@tuNombre".
+
+var carName = "Adrian";
 //2.Create a variable called x, assign the value 50 to it.
 var x = 50
 //3.mostrar con un alert la suma de 20+5(a+b)
-//4.Create a variable called z, y asignarle el valor de x+y y mostrar el resulado
-//5.En una sola linea declarar lo siguiente:
-    // firstName = "John"
-    // lastName = "Doe"
-    // age = 35
+var a = 20;
+var b = 5;
 
+alert("a+b = "+(a+b));
+//4.Create a variable called z, y asignarle el valor de x+y y mostrar el resulado
+var y = 30;
+alert("x+y = "+(x+y));
+//5.En una sola linea declarar lo siguiente:
+// firstName = "John"
+// lastName = "Doe"
+// age = 35
+firstName = "John";lastName = "Doe";age = 35
+alert(firstName+" "+ lastName +" tiene "+ age +" años")
 //6.miltiplicar 2 numeros(x,y) y mostrarlos
+alert("x*y = "+x*y);
 //7.Divide 10 y 2, mostrar resultados
+alert("10/2 = "+10/2);
 //8.mostrar el residuo de dividir 15/9
 
 
@@ -25,14 +36,18 @@ var x = 50
 //     firstName: "Brian",
 //     lastName: "Jacobo"
 //   };
-  
+
+var person = {
+    firstName: "Adrian",
+    lastName: "Capcha"
+};
+
+alert(person.firstName);
 //   alert(
 //   );
 //0. git pull
-    //git checkout -b rama-brianjacobo2
-    //recien hacen sus cambios
+//git checkout -b rama-brianjacobo2
+//recien hacen sus cambios
 //1. git add .
 //2. git commit - "tarea resuelta del 1 al 3"
 //3. git push
-
-////////////////////////////////////////////////////////////////aaaa
